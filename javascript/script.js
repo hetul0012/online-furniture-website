bc98a3e908d5b9638d16cecf73c2482037c82a6c
Hetul Suthar <hetulss3@gmail.com>
2023-10-28 18:27:54 +0530

    online-furniture-website

---
+++ javascript/script.js
@@ -0,0 +1,39 @@
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function(){
+    navBar = document.querySelector(".nav-bar")
+    navBar.classList.toggle("active")
+}
+
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    sliderPerGroup: 3,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+    },
+  });
